2f66460ef72faf493958caeca140a6f9739ed30f
Maike <maike.silva@adptivesoft.com.br>
2021-12-07 22:28:14 -0300

    Deploy

--- deploy.bat
+++ deploy.bat
@@ -1,13 +1,19 @@
-start 
+start
+	set echo off 
+	IF "%1."=="." ( 
+		SET @mensagem_commit="Commit sem mensagem"
+	) ELSE ( 
+		SET @mensagem_commit=%1
+	)
 
 	hugo
 	cd public
 	git add .
-	git commit -m "Commit sem mensagem"
+	git commit -m "%@mensagem_commit%"
 	git push origin main
 
 	cd ..
 	git add .
-	git commit -m "Commit sem mensagem"
+	git commit -m "%@mensagem_commit%"
 	git push origin main
 @pause
